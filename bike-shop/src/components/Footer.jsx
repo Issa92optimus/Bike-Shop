6e5d6980d713671eb9fb371ce0e053acfae890de
Issa Nyale <me@email.com>
2024-02-27 09:32:02 +0300

    Done with Footer

---
+++ bike-shop/src/components/Footer.jsx
@@ -0,0 +1,88 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="footer-content">
+            <ul className="footer-content__1">
+              <li>
+                <span>BIKE</span> Rental
+              </li>
+              <li>
+                We offers a big range of bikes for all your driving needs. We
+                have the perfect bike to meet your needs.
+              </li>
+              <li>
+                <a href="tel:254759515450">
+                  <i className="fa-solid fa-phone"></i> &nbsp; (254) -759-5154-50
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="mailto: 
+                bikerental@gmail.com"
+                >
+                  <i className="fa-solid fa-envelope"></i>
+                  &nbsp; bikerental@gmail.com
+                </a>
+              </li>
+
+              <li>
+                <a
+                  style={{ fontSize: "14px" }}
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://xpeedstudio.com/"
+                >
+                  Design by XpeedStudio
+                </a>
+              </li>
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Company</li>
+              <li>
+                <a href="#home">Nairobi</a>
+              </li>
+              <li>
+                <a href="#home">Careers</a>
+              </li>
+              <li>
+                <a href="#home">Mobile</a>
+              </li>
+              <li>
+                <a href="#home">Blog</a>
+              </li>
+              <li>
+                <a href="#home">How we work</a>
+              </li>
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Working Hours</li>
+              <li>Mon - Fri: 9:00AM - 5:00PM</li>
+              <li>Sat: 9:00AM - 4:00PM</li>
+              <li>Sun: Closed</li>
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Subscription</li>
+              <li>
+                <p>Subscribe your Email address for latest news & updates.</p>
+              </li>
+              <li>
+                <input type="email" placeholder="Enter Email Address"></input>
+              </li>
+              <li>
+                <button className="submit-email">Submit</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
